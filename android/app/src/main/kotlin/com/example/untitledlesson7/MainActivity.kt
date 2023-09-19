f175810c31fcc60316727a099734500d3e676383
Zeinab-emad <zeinab2003@gmail.com>
2023-09-19 22:38:54 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/untitledlesson7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.untitledlesson7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }}
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+//  var current2 = WordPair.random(); //عشان نضيف كلمتين لو ايزه 1 نحذف السطر التانى
+  void getNext() {
+    current = WordPair.random();
+   // current2 = WordPair.random(); //عشان نغير ف ال2 لو 1 نحذف التانى
+    notifyListeners();
+  }
+  // ↓ Add the code below.
+  var favorites = <WordPair>[];
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }}
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current; // ← Add this.
+    // ↓ Add this.
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('A random idea:'),
+              // Text(appState.current.asLowerCase),    // ← Change  this.
+              BigCard(pair : pair), // ← Change to this.
+             // Text(appState.current2.asLowerCase), //كده 2 تكست كلمتين
+              SizedBox(height: 10,),
+              Row(
+                mainAxisSize: MainAxisSize.min,   // ← Add this.
+                children: [
+                  // ↓ And this.
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      appState.toggleFavorite();
+                    },
+                    icon: Icon(icon),
+                    label: Text('Like'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      //print('button pressed!');
+                      appState.getNext();
+                    },
+                    child: Text('Next'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+  final WordPair pair;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); // ← Add this
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+          ),
+        ),
+      ),
+    );
+  }
+}
